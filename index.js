1452c80632ec07ee5c835ac6fc6bc1e6295820dd
BHObserver <BHObserver0@gmail.com>
2023-07-29 19:15:59 +0600

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,5 @@ hamburger.addEventListener('click', () => {
 });
 
 document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
-  hamburger.classList.remove('active');
-  navMenu.classList.remove('active');
+
 }));
