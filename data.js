504e3cfb9bd373db4b8150c63e036da67d8b2903
maserekafelex <maserekafelex1@gmail.com>
2023-08-03 15:38:53 +0300

    fix linter error

--- data.js
+++ data.js
@@ -1,26 +1,25 @@
-const form = document.querySelector('.form'); // get form by class name 
+const form = document.querySelector('.form'); // get form by class name
 // create formData object to to hold the input
 const formData = {
-    name: document.getElementById('name').value,
-    email: document.getElementById('email').value,
-    message: document.getElementById('message').value,
+  name: document.getElementById('name').value,
+  email: document.getElementById('email').value,
+  message: document.getElementById('message').value,
 };
 
-let getFormData = window.localStorage.getItem('formData'); // call to get data from localstorage. 
-// Check if there is any saved form data in localStorage.If there is set the values of the inputs fields to the values.
+let getFormData = window.localStorage.getItem('formData'); // call to get data from localstorage.
+// Check inputs fields to the values.
 if (getFormData) {
-    getFormData = JSON.parse(getFormData);
-    document.getElementById('name').value = getFormData.name;
-    document.getElementById('email').value = getFormData.email;
-    document.getElementById('message').value = getFormData.message;
+  getFormData = JSON.parse(getFormData);
+  document.getElementById('name').value = getFormData.name;
+  document.getElementById('email').value = getFormData.email;
+  document.getElementById('message').value = getFormData.message;
 }
-// array iterate through all of the elements in the form element and add an input event listener to each element.
+// array iterate through all of the eleme and event listener to each element.
 Array.from(form).forEach((element) => {
-    element.addEventListener('input', () => {
-        formData.name = document.getElementById('name').value
-        formData.email = document.getElementById('email').value
-        formData.message = document.getElementById('message').value;
-        localStorage.setItem('formData', JSON.stringify(formData));
-    });
+  element.addEventListener('input', () => {
+    formData.name = document.getElementById('name').value;
+    formData.email = document.getElementById('email').value;
+    formData.message = document.getElementById('message').value;
+    localStorage.setItem('formData', JSON.stringify(formData));
+  });
 });
-
